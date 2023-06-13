d4ad57828b39dc6759b5fc77279953df402cb4e8
Ngandos <76482435+Ngandos@users.noreply.github.com>
2023-06-13 04:07:54 +0200

    Revert "Merge pull request #2 from Ngandos/demo"
    This reverts commit 4c648a1d9c6de4d5dcbef9912ee340ac60ab2e67, reversing
    changes made to 208cd03df24429d04e52d45c2a9ab4e12efe255e.

--- src/main/java/fr/abbo/septArche/DAO/StockRepository.java
+++ src/main/java/fr/abbo/septArche/DAO/StockRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
@@ -12,5 +13,6 @@ public interface StockRepository extends JpaRepository<Stock, Long> {
     default List<Stock> findAll() {
         return null;
     }
-
+    @Override
+    Optional<Stock> findById(Long id);
 }
