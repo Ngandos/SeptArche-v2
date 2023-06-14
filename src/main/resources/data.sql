8023e86c48348585b35a50db08670c18bd0c230f
Ngandos <76482435+Ngandos@users.noreply.github.com>
2023-06-14 20:57:52 +0200

    WIP - Commande Management

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,6 +16,9 @@ INSERT INTO auteurs (id, nom) VALUES (9, 'Pierre Boulle');
 INSERT INTO auteurs (id, nom) VALUES (10, 'Georges RR Martin');
 INSERT INTO auteurs (id, nom) VALUES (11, 'Thomas Harris');
 INSERT INTO auteurs (id, nom) VALUES (12, 'Philipp K Dick');
+INSERT INTO auteurs (id, nom) VALUES (13, 'Steves Kloves');
+INSERT INTO auteurs (id, nom) VALUES (14, 'Tite Kubo');
+INSERT INTO auteurs (id, nom) VALUES (15, 'Chris Evans');
 
 -- Persistance Editeurs
 
@@ -26,17 +29,17 @@ INSERT INTO editeurs (id, Enseigne) VALUES (4, 'J ai lu');
 
 -- Persistance d'objets livre en tant que livre et en tant Qu'article
 
-INSERT INTO articles (id, category, prixHT, tva, ref, designation, stock) VALUES (7, 'Livres', 23.56, 0.2, 192021, 'Les misérables', 200);
-INSERT INTO livres (id, titre, editeur, isbn, id_auteur) VALUES (7, 'Les misérables', 'Gallimard', '1576VH', 1);
+INSERT INTO articles (id, category, prixHT, tva, ref, designation, stock) VALUES (7, 'Livres', 23.56, 0.2, 192021, 'Les miserables', 200);
+INSERT INTO livres (id, titre, editeur, isbn, id_auteur) VALUES (7, 'Les miserables', 'Gallimard', '1576VH', 1);
 
-INSERT INTO articles (id, category, prixHT, tva, ref, designation, stock) VALUES (8, 'Livres', 20.30, 0.2, 222324, 'Lestat le Vampire', 50);
-INSERT INTO livres (id, titre, editeur, isbn, id_auteur) VALUES (8, 'Lestat le Vampire', 'Gallimard', '1689AR', 3);
+INSERT INTO articles (id, category, prixHT, tva, ref, designation, stock) VALUES (8, 'Livres', 20.30, 0.2, 222324, 'Lestat le vampire', 50);
+INSERT INTO livres (id, titre, editeur, isbn, id_auteur) VALUES (8, 'Lestat le vampire', 'Gallimard', '1689AR', 3);
 
-INSERT INTO articles (id, category, prixHT, tva, ref, designation, stock) VALUES (9, 'Livres', 20.30, 0.2, '1789CB', 'Les Fleurs du mal', 76);
-INSERT INTO livres (id, titre, editeur, isbn, id_auteur) VALUES (9, 'Les Frleurs du mal', 'Edition de poche', '1789CB', 2);
+INSERT INTO articles (id, category, prixHT, tva, ref, designation, stock) VALUES (9, 'Livres', 20.30, 0.2, '1789CB', 'Bleach', 76);
+INSERT INTO livres (id, titre, editeur, isbn, id_auteur) VALUES (9, 'Bleach', 'Edition de poche', '1789TK', 14);
 
-INSERT INTO articles (id, category, prixHT, tva, ref, designation, stock) VALUES (10, 'Livres', 20.30, 0.2, '2345JBP', 'Le Malade Imaginaire', 32);
-INSERT INTO livres (id, titre, editeur, isbn, id_auteur) VALUES (10, 'Le Malade Imaginaire', 'Edition de poche', '2345JBP', 4);
+INSERT INTO articles (id, category, prixHT, tva, ref, designation, stock) VALUES (10, 'Livres', 20.30, 0.2, '2345JBP', 'Germinal', 32);
+INSERT INTO livres (id, titre, editeur, isbn, id_auteur) VALUES (10, 'Germinal', 'Edition de poche', '2345VH', 1);
 
 INSERT INTO articles (id, category, prixHT, tva, ref, designation, stock) VALUES (11, 'Livres', 20.30, 0.2, '3154JV', '20000 Lieues sous les mers', 40);
 INSERT INTO livres (id, titre, editeur, isbn, id_auteur) VALUES (11, '20000 Lieues sous les mers', 'Edition de poche', '3154JV', 6);
@@ -44,8 +47,8 @@ INSERT INTO livres (id, titre, editeur, isbn, id_auteur) VALUES (11, '20000 Lieu
 INSERT INTO articles (id, category, prixHT, tva, ref, designation, stock) VALUES (12, 'Livres', 20.30, 0.2, '123', 'Le seigneur des anneaux', 10 );
 INSERT INTO livres (id, titre, editeur, isbn, id_auteur) VALUES (12, 'Le seigneur des anneaux', 'Edition de poche', '3174TK', 7);
 
-INSERT INTO articles (id, category, prixHT, tva, ref, designation, stock) VALUES (13, 'Livres', 20.30, 0.2, '456', 'Entretien avec un vampire', 45);
-INSERT INTO livres (id, titre, editeur, isbn, id_auteur) VALUES (13, 'Entretien avec un vampire', 'Edition de poche', '3154AR', 3);
+INSERT INTO articles (id, category, prixHT, tva, ref, designation, stock) VALUES (13, 'Livres', 20.30, 0.2, '456', 'Snowpiercer', 45);
+INSERT INTO livres (id, titre, editeur, isbn, id_auteur) VALUES (13, 'Snowpiercer', 'Edition de poche', '3154CE', 15);
 
 INSERT INTO articles (id, category, prixHT, tva, ref, designation, stock) VALUES (14, 'Livres', 20.30, 0.2, '789', 'Le silence des agneaux', 100);
 INSERT INTO livres (id, titre, editeur, isbn, id_auteur) VALUES (14, 'Le silence des agneaux', 'Edition de poche', '3154TH', 11);
@@ -56,8 +59,8 @@ INSERT INTO livres (id, titre, editeur, isbn, id_auteur) VALUES (15, 'Le trône 
 INSERT INTO articles (id, category, prixHT, tva, ref, designation, stock) VALUES (16, 'Livres', 20.30,  0.2,   '131415', 'La planete des singes', 58);
 INSERT INTO livres (id, titre, editeur, isbn, id_auteur) VALUES (16, 'La planete des singes', 'Livre de poche', '3154PB', 9);
 
-INSERT INTO articles (id, category, prixHT, tva, ref, designation, stock) VALUES (17, 'Livres', 20.30, 0.2, '161718', 'Lhistoire de PI', 70);
-INSERT INTO livres (id, titre, editeur, isbn, id_auteur) VALUES (17, 'Lhistoire de PI', 'Edition de poche', '3154YM', 8);
+INSERT INTO articles (id, category, prixHT, tva, ref, designation, stock) VALUES (17, 'Livres', 20.30, 0.2, '161718', 'Harry Potter', 70);
+INSERT INTO livres (id, titre, editeur, isbn, id_auteur) VALUES (17, 'Harry Potter', 'Edition de poche', '3154YM', 13);
 
 INSERT INTO articles (id, category, prixHT, tva, ref, designation, stock) VALUES (18, 'Livres', 28, 0.2, '161719', 'Blade Runner', 70);
 INSERT INTO livres (id, titre, editeur, isbn, id_auteur) VALUES (18, 'Blade Runner', 'J ai lu', '3154PKD', 12);
