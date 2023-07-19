6d21108b50a02ea4bd2fe63757fcb87e7259d046
ABBO <76482435+Ngandos@users.noreply.github.com>
2023-07-19 12:21:12 +0200

    LigneCommande CRUD add

--- src/main/java/fr/abbo/septArche/Services/LigneCommandeServices.java
+++ src/main/java/fr/abbo/septArche/Services/LigneCommandeServices.java
@@ -7,6 +7,11 @@ import fr.abbo.septArche.models.LigneCommande;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LigneCommandeServices {
@@ -14,6 +19,15 @@ public class LigneCommandeServices {
     private ArticlesRepository repArticles;
 
     private LigneCommandeServices repLigneCommande;
+
+    @GetMapping("/ligne_commande")
+    public List<LigneCommande> findAll(@RequestParam Long id) {
+        return repLigneCommande.findAll(id);
+    }
+    @GetMapping("/{id}")
+    public Optional<LigneCommande> findById(Long id) {
+        return repLigneCommande.findById(id);
+    }
     @Transactional(rollbackFor = StockExceptions.class)
     public void creerLigneCommande(Articles articles, int qte) throws StockExceptions {
         LigneCommande LigneCommande = new LigneCommande();
