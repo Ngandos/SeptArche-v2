324f84a7f9bc6da7142655468e4956e4a658ee56
ABBO <76482435+Ngandos@users.noreply.github.com>
2023-09-06 16:11:19 +0200

    Bug Users Update fix

--- src/main/java/fr/abbo/septArche/controllers/AuthentificationController.java
+++ src/main/java/fr/abbo/septArche/controllers/AuthentificationController.java
@@ -1,6 +1,6 @@
 package fr.abbo.septArche.controllers;
 
-import fr.abbo.septArche.models.User;
+import fr.abbo.septArche.models.Users;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.*;
 public class AuthentificationController {
 
     @PostMapping()
-    public User login(@RequestBody User user) {
+    public Users login(@RequestBody Users users) {
         // Si on arrive dans cette méthode c'est qu'on est authentifié (Spring Security nous bloque sinon)
-        System.out.println("login de : " + user.getUserName());
-        return user;
+        System.out.println("login de : " + users.getUsername());
+        return users;
         // retourne l'utilisateur connecté pour que React soit au courant que l'authentification est bonne
     }
 }
