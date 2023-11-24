042735446e7a96b945f40a0e31748741f6adb17f
Ngandos <76482435+Ngandos@users.noreply.github.com>
2023-11-24 14:22:17 +0100

    AppPropertiesFix

--- src/main/java/fr/abbo/septArche/SeptArcheApplication.java
+++ src/main/java/fr/abbo/septArche/SeptArcheApplication.java
@@ -12,6 +12,18 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class SeptArcheApplication {
 	public static void main(String[] args) {
 
+		System.setProperty("server.port", "8080");
+		System.setProperty("server.servlet.context-path", "/demo");
+		System.setProperty("spring.datasource.url", "jdbc:mysql://localhost:8889/7eArcheDB?createDatabaseIfNotExist=true");
+		System.setProperty("spring.jpa.show-sql", "true");
+		System.setProperty("spring.jpa.hibernate.ddl-auto", "update");
+		System.setProperty("spring.datasource.username", "root");
+		System.setProperty("spring.datasource.password", "root");
+		System.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+		System.setProperty("logging.level.root", "INFO");
+		System.setProperty("spring.jpa.defer-datasource-initialization", "true");
+		System.setProperty("logging.level.fr.abbo.septArche", "DEBUG");
+
 		SpringApplication.run(SeptArcheApplication.class, args);
 
 	}
